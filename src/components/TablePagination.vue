f4568a7cd7ddf5e931241c551b32531f1d79003d
slim-python <xl4bhi@gmail.com>
2022-04-10 19:20:51 +0530

    final code

---
+++ src/components/TablePagination.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <nav aria-label="Page navigation example ">
+      <ul class="inline-flex -space-x-px">
+        <li>
+          <a
+            @click="$store.commit('decreasePageNumber')"
+            href="#"
+            class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >Previous</a
+          >
+        </li>
+
+        <li>
+          <a
+            @click="$store.commit('increasePageNumber')"
+            href="#"
+            class="py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >Next</a
+          >
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TablePagination",
+  data() {
+    return {
+      pageNumber: 0,
+    };
+  },
+};
+</script>
